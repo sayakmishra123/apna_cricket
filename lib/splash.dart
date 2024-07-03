64d451537d65ade0d411125539f360a35f69fa62
sayakflutter <sayak.solution27@gmail.com>
2024-07-03 10:44:13 +0530

    splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -11,10 +11,28 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(
-        'assets/splash.jpg',
-        fit: BoxFit.fill,
-      ),
+      body: Stack(children: [
+        SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          child: Image.asset(
+            'assets/splash.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Apna Cricket',
+                style: TextStyle(color: Colors.white),
+                textScaler: TextScaler.linear(2),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
